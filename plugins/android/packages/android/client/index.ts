8a651dfbe94b0ba71932b7b739e240cf404aaa92
clinal <clinal@users.noreply.github.com>
2026-08-02 19:52:05 +0000

    ANI-29: align Cordis WebUI development stack
    Co-authored-by: Anillc <void@anil.lc>

--- plugins/android/packages/android/client/index.ts
+++ plugins/android/packages/android/client/index.ts
@@ -1,9 +1,10 @@
 import { Context } from '@cordisjs/client'
+import type {} from '@cordisjs/plugin-loader-webui/client'
 import Debug from './debug.vue'
 
 export default (ctx: Context) => {
   ctx.inject(['manager'], (ctx) => {
-    ctx.slot({
+    ctx.client.router.slot({
       type: 'plugin-details',
       component: Debug,
       order: 0,
